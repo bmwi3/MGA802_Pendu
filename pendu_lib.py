61dac351cafeefbf9338b6b050ad0a9aae2faaaa
bmwi3 <julien.giner@utbm.fr>
2025-05-15 15:30:34 -0400

    Version avec librairie créée

---
+++ pendu_lib.py
@@ -0,0 +1,220 @@
+def afficher_pendu(essais_restants):
+    etats = [
+        """
+         +---+
+         |   |
+         O   |
+        /|\\  |
+        / \\  |
+             |
+        =========
+        """,
+        """
+         +---+
+         |   |
+         O   |
+        /|\\  |
+        /    |
+             |
+        =========
+        """,
+        """
+         +---+
+         |   |
+         O   |
+        /|\\  |
+             |
+             |
+        =========
+        """,
+        """
+         +---+
+         |   |
+         O   |
+        /|   |
+             |
+             |
+        =========
+        """,
+        """
+         +---+
+         |   |
+         O   |
+         |   |
+             |
+             |
+        =========
+        """,
+        """
+         +---+
+         |   |
+         O   |
+             |
+             |
+             |
+        =========
+        """,
+        """
+         +---+
+         |   |
+             |
+             |
+             |
+             |
+        =========
+        """
+    ]
+    # Affiche l'état correspondant au nombre d'essais restants
+    print(etats[essais_restants])
+
+
+
+
+
+def init_path():
+    path=input("Entrez le chemin du fichier de mots (laisser vide pour le fichier par défaut) : ")
+    if path == '':
+        path = None
+    return path
+
+def ouvrir_fichier(path):
+    if path == None:
+        try:
+            with open("mots_pendu.txt", 'r',encoding='utf-8') as mots_pendu:
+                mots = [ligne.strip() for ligne in mots_pendu if ligne.strip()]
+            return mots
+        except FileNotFoundError:
+            print("Le fichier 'mots_pendu.txt' n'a pas été trouvé.")
+            return False
+    elif path:
+        print('path')
+        try:
+            with open(path, 'r',encoding='utf-8') as mots_pendu:
+                mots = [ligne.strip() for ligne in mots_pendu if ligne.strip()]
+            return mots
+        except FileNotFoundError:
+            print("Le fichier sélectionné n'a pas été trouvé.")
+            return False
+
+def remplacer_accent(mot):
+    accents = {
+        'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e',
+        'à': 'a', 'â': 'a', 'ä': 'a',
+        'ô': 'o', 'ö': 'o',
+        'ç': 'c',
+        'ù': 'u', 'û': 'u', 'ü': 'u',
+        'î': 'i', 'ï': 'i',
+    }
+    for accent, replacement in accents.items():
+        mot = mot.replace(accent, replacement)
+    return mot
+
+
+def choisir_mot(mots):
+    return random.choice(mots)
+
+
+def affichage_under_score(mot):
+    return '_' * len(mot)
+
+
+def entrer_lettre():
+    while True:
+        lettre = input("Entrez une lettre : ")
+        if len(lettre) == 1 and lettre.isalpha():
+            return lettre.lower()
+        else:
+            print("Veuillez entrer une seule lettre.")
+
+def verifier_gagne(mot_cache, mot_aleatoire):
+    if mot_cache == mot_aleatoire:
+        print("Vous avez gagné ! Le mot était :", mot_aleatoire)
+        return True
+    else:
+        return False
+
+def hint(mot_aleatoire, essais_restants, mot_cache,indice_donne):
+    if indice_donne:
+        return True
+    elif essais_restants == 1 and mot_cache[0] == '_':
+        # Afficher la première lettre du mot
+        print("Indice : Le mot commence par la lettre '", mot_aleatoire[0], "'")
+        return True
+    elif essais_restants == 1:
+        # Afficher une lettre aléatoire du mot
+        index = random.randint(0, len(mot_aleatoire) - 1)
+        while mot_cache[index] != '_':
+            index = random.randint(0, len(mot_aleatoire) - 1)
+
+        print(f"Indice : La lettre à la position {index + 1} est '{mot_aleatoire[index]}'.")
+        mot_cache = mot_cache[:index] + mot_aleatoire[index] + mot_cache[index + 1:]
+        print("Mot : ", mot_cache)
+        return True
+    return indice_donne
+
+def check_lettre(lettre,mot_aleatoire,mot_cache,essais_restants):
+    if lettre in mot_aleatoire:
+        print("Bien joué ! La lettre est dans le mot.")
+        for j in range(len(mot_aleatoire)):
+            if mot_aleatoire[j] == lettre:
+                mot_cache = mot_cache[:j] + lettre + mot_cache[j+1:]
+        print("Mot : ", mot_cache)
+    else:
+        print("Dommage ! La lettre n'est pas dans le mot.")
+        print("Vous avez perdu un essai.")
+        essais_restants -= 1
+    return essais_restants,mot_cache
+
+def replay():
+    while True:
+        replay = input("Voulez-vous rejouer ? (O/N) : ")
+        if len(replay) == 1 and replay.isalpha():
+            replay=replay.lower()
+            break
+        else:
+            print("Veuillez entrer une seule lettre.")
+
+    if replay.lower() == 'o':
+        return init(mots)
+    else:
+        print("Merci d'avoir joué !")
+        return 0, None, None, None
+
+
+
+
+def acceuil():
+    print("=======================================")
+    print("      Bienvenue dans le jeu du Pendu ! ")
+    print("=======================================")
+    print("         +---+")
+    print("         |   |")
+    print("         O   |")
+    print("        /|\\  |")
+    print("        / \\  |")
+    print("             |")
+    print("      =========")
+    print("Règles :")
+    print("- Devinez le mot mystère en proposant une lettre à la fois.")
+    print("- Vous avez 6 essais pour trouver le mot complet.")
+    print("- Les accents sont remplacés par leur équivalent sans accent.")
+    print("- Entrez une seule lettre à chaque essai.")
+    print("---------------------------------------")
+    path = init_path()
+    mots=ouvrir_fichier(path)
+    if mots== False:
+        print("Aucun mot disponible pour le jeu.")
+        return False
+    else:
+        return mots
+
+
+def init(mots):
+    essais_restants = 6
+    indice_donne = False
+    mot_aleatoire = choisir_mot(mots)
+    mot_aleatoire = remplacer_accent(mot_aleatoire)
+    print(f"Le mot à deviner est : ", mot_aleatoire)
+    mot_cache = affichage_under_score(mot_aleatoire)
+    return essais_restants, mot_aleatoire, mot_cache,indice_donne
+
