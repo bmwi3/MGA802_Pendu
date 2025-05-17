7cf32541c6ede8d4252928a2e7d7334cb762a325
bmwi3 <julien.giner@utbm.fr>
2025-05-17 17:10:04 -0400

    Supprime le fichier pendu.py

--- pendu.py
+++
@@ -1,7 +0,0 @@
-import random
-import pendu_lib 
-from pendu_lib import acceuil, init, hint, entrer_lettre, check_lettre, verifier_gagne, afficher_pendu, replay, pendu
-
-
-if __name__ == "__main__":
-    pendu()
