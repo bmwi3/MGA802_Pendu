768046a84f291e820eca53485ba20797e48e06a0
bmwi3 <julien.giner@utbm.fr>
2025-05-17 14:27:08 -0400

    Option 2 fichiers un main

--- pendu_lib.py
+++ pendu_lib.py
@@ -218,3 +218,25 @@ def init(mots):
     mot_cache = affichage_under_score(mot_aleatoire)
     return essais_restants, mot_aleatoire, mot_cache,indice_donne
 
+def pendu():
+    mots=acceuil()
+
+    if mots== False:
+        print("Aucun mot disponible pour le jeu.")
+        exit()
+    essais_restants, mot_aleatoire, mot_cache,indice_donne=init(mots)
+
+    while essais_restants > 0:
+        indice_donne=hint(mot_aleatoire, essais_restants, mot_cache,indice_donne)
+        print("Il vous reste ", essais_restants, " essais")
+        afficher_pendu(essais_restants)
+        lettre = entrer_lettre()
+        
+
+        essais_restants,mot_cache=check_lettre(lettre,mot_aleatoire,mot_cache,essais_restants)
+        if verifier_gagne(mot_cache, mot_aleatoire) or essais_restants == 0:
+            if essais_restants == 0:
+                print("Vous avez perdu ! Le mot était :", mot_aleatoire)
+                afficher_pendu(essais_restants)
+
+            essais_restants,mot_aleatoire,mot_cache,indice_donne=replay()
